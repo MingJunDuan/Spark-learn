5470b0f468eb6a9a3997ae04dda497d43272b4a8
MingJunDuan <mjduan@yahoo.com>
2017-06-05 06:41:17 -0700

    [1.0.0] add spark-connect mysql

---
+++ src/main/java/com/mjduan/project/App.java
@@ -0,0 +1,35 @@
+package com.mjduan.project;
+
+import java.util.Properties;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SQLContext;
+
+/**
+ * Hans on 2017-06-01 07:12
+ */
+public class App {
+    private static final String URL = "jdbc:mysql://localhost:3306/mybank2?useUnicode=true&characterEncoding=UTF-8";
+
+    public static void main(String[] args){
+        SparkConf sparkConf = new SparkConf();
+        sparkConf.setAppName("spark-mysql");
+        sparkConf.setMaster("local[4]");
+
+        JavaSparkContext sparkContext = new JavaSparkContext(sparkConf);
+        SQLContext sqlContext = new SQLContext(sparkContext);
+
+        Properties connectionProperties = new Properties();
+        connectionProperties.setProperty("user", "root");// 设置用户名
+        connectionProperties.setProperty("password", "dmj2010");// 设置密码
+
+        Dataset<Row> mypay = sqlContext.read().jdbc(URL, "t_mypay", connectionProperties);
+        mypay.show(3);
+
+        sparkContext.close();
+    }
+
+}
